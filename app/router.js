3bb0c1614aabe340809ac0bd3fc3a246949db4ba
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-05 15:59:23 -0800

    nest vote route in index so top-nav is present

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
-  this.route('vote');
+  this.route('index', {path: '/'}, function () {
+  	this.route('vote', {path: '/vote'})
+  });
 });
 
 export default Router;
