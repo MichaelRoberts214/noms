cd30d185cbae6565ae9f048b98a6eb10053d2260
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-05 16:31:10 -0800

    generate vote modal component

---
+++ tests/integration/components/vote-modal-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('vote-modal', 'Integration | Component | vote modal', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{vote-modal}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#vote-modal}}
+      template block text
+    {{/vote-modal}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
