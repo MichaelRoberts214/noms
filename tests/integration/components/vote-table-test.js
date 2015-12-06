72e630a546fc4dbe88166d8cdaaa8ec5caea1a50
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-05 16:08:28 -0800

    generate vote-table component

---
+++ tests/integration/components/vote-table-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('vote-table', 'Integration | Component | vote table', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{vote-table}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#vote-table}}
+      template block text
+    {{/vote-table}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
