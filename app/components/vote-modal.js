5b0ccb78209f8abc24abf88d59de4042215367b5
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-07 13:43:36 -0800

    create placeholder submit action

--- app/components/vote-modal.js
+++ app/components/vote-modal.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	list: null,
+
 	actions: {
 		hideModal: function () {
 			this.toggleProperty('showModal');
+		},
+
+		submit: function () {
+			var list = this.get('list');
+			this.send('hideModal');
 		}
 	}
 });
